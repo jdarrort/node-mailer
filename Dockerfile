46bd7cb386c5e7561c9091f55dea830c944458e6
jdarrort <jdarrort@gmail.com>
2021-05-08 19:37:36 +0200

    upgrades

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM node:10-alpine
+FROM node:14-alpine
 RUN mkdir -p /opt/node_modules && chown -R node:node /opt
 WORKDIR /opt
 USER node
 COPY --chown=node:node . .
 RUN npm install
+
+ENV TZ /Europe/Paris
 
 CMD [ "node", "index.js" ]
 
--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 require('jd-node-env');
 const nodeMailer = require('nodemailer');
 const express = require("express");
-const bodyParser = require('body-parser')
+const DItf = require('./lib/d4itf');
+const LOGGER = require('./lib/logger');
 
 let required_env = [ "SMTP_USER_LOGIN", "SMTP_USER_PWD", "DEFAULT_RECIPIENT"];
 required_env.map( e => {
-    if (! process.env[e]) { console.warn(` Env var ${e} is required`); process.exit()};
+    if (! process.env[e]) { LOGGER.warn(` Env var ${e} is required`); process.exit()};
 })
 
 const MAX_SIMULTANEAOUS = process.env.MAX_SIMULTANEAOUS || 5;
 var nb_ongoing = 0;
 
 const app = express();
-app.use(bodyParser.json())
-
+app.use( express.json() );
+//----------------------------------------------
 let transporter = nodeMailer.createTransport({
     host: process.env.SMTP_SERVER,
     port: process.env.SMTP_PORT || 465,
     tls: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
+        ignoreTLS :true
     },
     secure: true,
     auth: {
@@ -27,19 +29,36 @@ let transporter = nodeMailer.createTransport({
         pass: process.env.SMTP_USER_PWD
     }
 });
-
-
+//----------------------------------------------
+DItf.registerRules("Mail","post",
+`   subject;       string;   Y
+     to;           string;   N
+     bcc;           string;   N
+     text;         string;   Y
+     attachments;  array;   N`);
+DItf.registerRules("Mail","attachment",
+     `  name;       string;   Y
+        datab64;    string;   Y`);
+//----------------------------------------------
 app.post('/mail', (req, res) => {
-    if (! req.body.subject || ! req.body.text) {
-        res.status(400).json( { error:"invalid attributes" } );
-        return;
+    let inputs  = DItf.buildItfData("Mail","post",req.body);
+    if (inputs.attachments) {
+        inputs.attachments.forEach( (att, i) => {
+            att = DItf.buildItfData("Mail","attachment",att);
+            inputs.attachments[i] = {
+                filename: att.name,
+                content : Buffer.from(att.datab64, 'base64')
+            }
+        })
     }
 
     let mail = {
-        from: `"RWS Node Mailer" <${process.env.SMTP_USER_EMAIL}>`, // sender address
-        to: req.body.to || process.env.DEFAULT_RECIPIENT,   
-        subject: req.body.subject,
-        text: req.body.text
+        from: `"Julien Darrort" <${process.env.SMTP_USER_EMAIL}>`, // sender address
+        to: inputs.to || process.env.DEFAULT_RECIPIENT,   
+        bcc: inputs.bcc || null,
+        subject: inputs.subject,
+        text: inputs.text,
+        attachments : inputs.attachments || []
     };
     
     if (nb_ongoing >= MAX_SIMULTANEAOUS) { 
@@ -47,34 +66,38 @@ app.post('/mail', (req, res) => {
         return;
     }
     nb_ongoing ++ ;
-    console.log('About to send Message to %s : %s', mail.to, mail.subject);
+    LOGGER.log(`About to send Message to ${mail.to} : ${mail.subject}`);
     transporter.sendMail(mail, (error, info) => {
         nb_ongoing -- ;
         if (error) {
-            return console.log(error);
+            LOGGER.log(error);
+            return res.status(500).json({ error : "Message non délivré  : " + error.message});
         }
-        console.log('Message %s sent: %s', info.messageId, info.response);
+        LOGGER.log(`Message  ${info.messageId} Sent : ${info.response}`);
+        return res.json({ status:"success" });    
     });
-    res.json({ status:"success" });    
 });
 
+//----------------------------------------------
 app.use((req, res, next) => {
     res.status(404).json({ error : "Invalid path"});
 });
+//----------------------------------------------
 app.use((error, req, res, next) => {
-    console.log(error);
+    LOGGER.log(error);
     res.status(500).json({ error : "server error " + error.message});
 });
 
+//----------------------------------------------
 transporter.verify(function(error, success) {
     if (error) {
-      console.log(error);
-      console.warn("Unable to connect to SMTP server. Exit");
+      LOGGER.log(error);
+      LOGGER.warn("Unable to connect to SMTP server. Exit");
       process.exit();
     }
-    console.log(`Connectivity to SMTP server '${process.env.SMTP_SERVER}' successful `);
+    LOGGER.log(`Connectivity to SMTP server '${process.env.SMTP_SERVER}' successful `);
     let port = process.env.NODE_PORT ||8100;
     app.listen( port, () =>
-      console.log(`node-mailer listening on port ${port}  for user ${process.env.SMTP_USER_EMAIL}!`),
+      LOGGER.log(`node-mailer listening on port ${port}  for user ${process.env.SMTP_USER_EMAIL}!`),
     );
 });
